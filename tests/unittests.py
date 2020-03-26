344102f98e01f40495d1d1640f9bd8de1a1e2e79
josh <joshsmith2@gmail.com>
2020-03-26 15:54:34 +0000

    Removed old singleclick test

--- tests/unittests.py
+++ tests/unittests.py
@@ -54,17 +54,6 @@ class UnitTest(unittest.TestCase):
         polis_container = self.driver.find_element_by_id(self.polis_iframe)
         self.assertTrue(polis_container.is_displayed())
 
-    def test_dynata_button_isnt_visible_until_iframe_clicked(self):
-        self.driver.get(self.url_query)
-        time.sleep(5)
-
-        voted_button = self.driver.find_elements_by_id('voted')
-        self.assertEqual(0, len(voted_button), "Can see button but shouldn't")
-
-        self.click_within_poll()
-        voted_button = self.driver.find_elements_by_id('voted')
-        self.assertEqual(1, len(voted_button), "Can't see button but want to")
-
     def test_dynata_button_isnt_visible_until_iframe_clicked_10_times(self):
         self.driver.get(self.url_query)
 
